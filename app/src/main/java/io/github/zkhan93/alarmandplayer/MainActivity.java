7955d45dd3d3d0e92dbb284f828ab69d8c02750f
Zeeshan Khan <zkhan1093@gmail.com>
2019-05-29 22:02:17 +0530

    recovery from connectivity failures

--- app/src/main/java/io/github/zkhan93/alarmandplayer/MainActivity.java
+++ app/src/main/java/io/github/zkhan93/alarmandplayer/MainActivity.java
@@ -1,7 +1,12 @@
 package io.github.zkhan93.alarmandplayer;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.preference.DialogPreference;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
@@ -67,14 +72,29 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.city)
     public TextView city;
 
+    @BindView(R.id.weather_views)
+    public View weatherViews;
+
     private SharedPreferences preferences;
     private View.OnClickListener cityClickedListener;
+    private ConnectivityManager.NetworkCallback connectivityListener;
+    private ConnectivityManager connectivityManager;
+    private AlarmManager alarmManager;
+
 
     {
         cityClickedListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "change city", Toast.LENGTH_SHORT).show();
+            }
+        };
+        connectivityListener = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(Network network) {
+                super.onAvailable(network);
+                connectivityManager.unregisterNetworkCallback(connectivityListener);
+                fetchWeatherInfo();
             }
         };
     }
@@ -88,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         weatherService = WeatherClient.getInstance().create(WeatherService.class);
         preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         ButterKnife.bind(this);
         city.setOnClickListener(cityClickedListener);
     }
@@ -95,30 +117,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        fetchWeatherInfo();
+        setUpAlarmToUpdateWeatherinfo();
+    }
+
+    private void fetchWeatherInfo() {
         String city = preferences.getString("city", "Bangalore,IN");
         weatherService.getWeather(city).enqueue(new Callback<WeatherResponse>() {
             @Override
             public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
-                Log.d(TAG,String.format("response: %s", response.body()));
-
+                Log.d(TAG, String.format("response: %s", response.body()));
                 WeatherResponse wres = response.body();
+                if (wres == null) {
+                    Log.e(TAG, "response from api was either blank or I couldn't parse it");
+                    return;
+                }
                 String icon = "";
                 String description = "";
-                for(Weather weather: wres.weather){
-                    int icon_unicode_id = getResources().getIdentifier(String.format("wi_%d", weather.id), "string", getPackageName());
+                for (Weather weather : wres.weather) {
+                    int icon_unicode_id = getResources().getIdentifier(String.format("wi_%d",
+                            weather.id), "string", getPackageName());
                     icon = getString(icon_unicode_id);
                     description = weather.description;
                 }
                 float temp_degree = wres.main.temp;
-                weatherMain.setText(getString(R.string.weather_main, (int)temp_degree, icon));
+                weatherMain.setText(getString(R.string.weather_main, (int) temp_degree, icon));
                 weatherDesc.setText(description);
-                weatherTempMinMax.setText(String.format("%.0f\u00B0 / %.0f\u00B0", wres.main.temp_min, wres.main.temp_max));
+                weatherTempMinMax.setText(String.format("%.0f\u00B0 / %.0f\u00B0",
+                        wres.main.temp_min, wres.main.temp_max));
+                weatherViews.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onFailure(Call<WeatherResponse> call, Throwable t) {
-                Log.e(TAG, String.format("error calling openweather API: %s\n%s", t.getMessage(), ErrorUtils.getLogTrace(t)));
+                Log.e(TAG, String.format("error calling openweather API: %s\n%s", t.getMessage(),
+                        ErrorUtils.getLogTrace(t)));
+                watchConnectivity();
             }
         });
+    }
+
+    private void watchConnectivity() {
+        connectivityManager.registerDefaultNetworkCallback(connectivityListener);
+    }
+    private void  setUpAlarmToUpdateWeatherinfo(){
+//        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, );
     }
 }
---
+++ app/src/main/java/io/github/zkhan93/alarmandplayer/listener/ConnectivityListener.java
@@ -0,0 +1,13 @@
+package io.github.zkhan93.alarmandplayer.listener;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ConnectivityListener extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
